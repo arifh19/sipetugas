edab24d4ab58b3c39ad496ea9194a99aeb49684c
Arif Hidayat <arif.h@mail.ugm.ac.id>
2018-12-20 11:29:26 +0700

    hehe

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,6 +14,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+	\URL::forceScheme('https');
+	if($this->app->environment() === 'production'){ 
+	$this->app['request']->server->set('HTTPS', true); }
         Schema::defaultStringLength(191);
     }
 
